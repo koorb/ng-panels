b6ea1674827dbc6ff69c42d23826399faa831af7
Richard Standbrook <richard@themilkshed.co.uk>
2014-06-04 11:27:42 +0100

    fix: Adds cross browser properties to .active
    Also namespaces classes.

--- ng-panels.js
+++ ng-panels.js
@@ -4,13 +4,14 @@ angular.module('ngPanels', [])
     return {
       restrict: 'EA',
       transclude: true,
-      template: '<div class="navigation-mask" ng-click="panelOptions.unmask()" ng-if="panelOptions.masked"></div><div ng-transclude></div>',
+      template: '<div class="ng-panel-mask" ng-click="panelOptions.unmask()" ng-if="panelOptions.masked"></div><div ng-transclude></div>',
       scope: {
         panelOptions: '=',
         path: '='
       },
       controller: function($scope, $element, $attrs) {
-  
+        $element.addClass('ng-panel-view');
+
         $scope.$watch('panelOptions.active', function (isActive, wasActive) {
           if (isActive) {
             $element.addClass('active');
@@ -18,17 +19,21 @@ angular.module('ngPanels', [])
             $element.removeClass('active');
           }
         });
-        
+
         $scope.$watch('panelOptions.masked', function (isMasked, wasMasked) {
           if (isMasked) {
             $element.addClass('masked');
             if ($element.css('right') !== 'auto') {
               var x = $element.parent().width();
+              $element.css('transform', 'translate3d(-'+x+'px, 0, 0)');
               $element.css('-webkit-transform', 'translate3d(-'+x+'px, 0, 0)');
+              $element.css('-moz-transform', 'translate3d(-'+x+'px, 0, 0)');
             }
           } else {
             $element.removeClass('masked');
+            $element.css('transform', '');
             $element.css('-webkit-transform', '');
+            $element.css('-moz-transform', '');
           }
         });
       }
