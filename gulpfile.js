a3ea008c2f5b0b13dc467658d2876fe50db53bc2
Richard Standbrook <richstandbrook@icloud.com>
2015-01-19 13:33:09 +0000

    chore: Updates build script

--- gulpfile.js
+++ gulpfile.js
@@ -6,15 +6,30 @@
       less = require('gulp-less'),
       ngAnnotate = require('gulp-ng-annotate');
 
-  gulp.task('build', function() {
-    gulp.src('./src/*.js')
-      .pipe(concat('ng-panel.js'))
-      .pipe(ngAnnotate())
-      .pipe(gulp.dest('./dist/'));
+  var paths = {
+    scripts: './src/*.js',
+    styles: './src/*.less',
+    dest: './dist/'
+  };
 
-    gulp.src('./src/*.less')
-      .pipe(less())
-      .pipe(gulp.dest('./dist/'));
+  gulp.task('scripts', function() {
+    gulp.src(paths.scripts)
+      .pipe(concat('ng-panels.js'))
+      .pipe(ngAnnotate())
+      .pipe(gulp.dest(paths.dest));
   });
+
+  gulp.task('styles', function() {
+    gulp.src(paths.styles)
+      .pipe(less())
+      .pipe(gulp.dest(paths.dest));
+  });
+
+  gulp.task('watch', function() {
+    gulp.watch(paths.scripts, ['scripts']);
+    gulp.watch(paths.styles, ['styles']);
+  });
+
+  gulp.task('default', ['scripts', 'styles']);
 
 })();
