d263253ef8ff72aa285aea269e2f9ba64a30b378
Richard Standbrook <richstandbrook@icloud.com>
2015-01-19 13:47:31 +0000

    feat: Updates example to use new Service

--- example/app.js
+++ example/app.js
@@ -1,55 +1,35 @@
-var app = angular.module('ngPanelsDemo', ['ngPanels']);
+(function(){
+  'use strict';
 
-app.controller('MainCtrl', function($scope) {
-  
-  $scope.panels = {
-    one: {
-      masked: false
-    },
-    two: {
-      active: false,
-      masked: false
-    },
-    three: {
-      active: false,
-      masked: false
-    },
-    four: {
-      active: false,
-      masked: false
-    }
-  };
-  
-  $scope.openPanelTwo = function() {
-    $scope.panels.two.active = true;
-    $scope.panels.one.masked = true;
-    $scope.panels.one.unmask = $scope.closePanelTwo;
-  };
-  
-  $scope.closePanelTwo = function() {
-    $scope.panels.two.active = false;
-    $scope.panels.one.masked = false;
-  };
-  
-  $scope.openPanelThree = function() {
-    $scope.panels.three.active = true;
-    $scope.panels.two.masked = true;
-    $scope.panels.two.unmask = $scope.closePanelThree;
-  };
-  
-  $scope.closePanelThree = function() {
-    $scope.panels.three.active = false;
-    $scope.panels.two.masked = false;
-  };
-  
-  $scope.openPanelFour = function() {
-    $scope.panels.four.active = true;
-    $scope.panels.three.masked = true;
-    $scope.panels.three.unmask = $scope.closePanelFour;
-  };
-  
-  $scope.closePanelFour = function() {
-    $scope.panels.four.active = false;
-    $scope.panels.three.masked = false;
-  };
-});
+  angular.module('ngPanelsDemo', ['ngPanels'])
+
+    .controller('mainCtrl', function($scope, ngPanels) {
+
+      $scope.panels = ngPanels.newGroup('demo', {
+        one: {},
+        two: {
+          masks: ['one']
+        },
+        three: {
+          masks: ['two']
+        },
+        four: {
+          masks: ['three']
+        }
+      });
+
+      $scope.openPanelTwo = function() {
+        $scope.panels.open('two');
+      };
+
+      $scope.openPanelThree = function() {
+        $scope.panels.open('three');
+      };
+
+      $scope.openPanelFour = function() {
+        $scope.panels.open('four');
+      };
+
+    });
+
+})();
